a2c38eae5ad2d12d7f394a6e60468406d6ba3d94
vagrant <vagrant@vccw.dev>
2017-05-31 18:28:40 +0000

    Update filesystem helper

--- includes/filesystem_helper.php
+++ includes/filesystem_helper.php
@@ -7,30 +7,29 @@ class Filesystem_Helper {
   
   private function __construct( $path ) {
     $this->set_path( $path );
-    $this->initialize_filesystem( $path );
   }
 
-  private function initialize_filesystem() {
-    
-    ob_start();
-    $credentials = request_filesystem_credentials( $this->path );
-    $data = ob_get_clean(); 
-    if ( false === $credentials ) {
-	return $this->check_connection( $data );
-    }
- 
-    if ( ! WP_Filesystem( $credentials ) ) {
-        ob_start();
-        request_filesystem_credentials( $this->path, '', true ); // Failed to connect, Error and request again.
-        $data = ob_get_clean();
-	return $this->check_connection( $data );
-    }
+  private function initialize_filesystem() {  
 
-    if ( ! is_object($wp_filesystem) )
-        return new WP_Error('fs_unavailable', __('Could not access filesystem.'));
+    global $wp_filesystem;
+
+    ob_start();
+    $credentials = request_filesystem_credentials( '', '', false, $this->path, null, true );
+    $data = ob_get_clean();
  
-    if ( is_wp_error($wp_filesystem->errors) && $wp_filesystem->errors->get_error_code() )
-        return new WP_Error('fs_error', __('Filesystem error.'), $wp_filesystem->errors);
+    if ( false === $credentials || ( ! WP_Filesystem( $credentials, $this->path, true ) )) {
+        return new WP_Error( 'fs', __( 'Filesystem API could not be initialized.', 'nginx-cache' ) );
+    }
+    if ( ! $wp_filesystem->exists( $path ) ) {
+	return new WP_Error( 'fs', __( '"Cache Zone Path" does not exist.', 'nginx-cache' ) );
+    }
+    if ( ! $wp_filesystem->is_dir( $path ) ) {
+	return new WP_Error( 'fs', __( '"Cache Zone Path" is not a directory.', 'nginx-cache' ) );
+    }
+    if ( ! $wp_filesystem->is_writable( $path ) ) {
+	return new WP_Error( 'fs', __( '"Cache Zone Path" is not writable.', 'nginx-cache' ) );
+    }
+    return;
   }
 
   private function set_path( $path ) {
@@ -41,26 +40,15 @@ class Filesystem_Helper {
     return;
   }
 
-  private function check_connection( $data ) {
-        if ( ! empty($data) ){
-            include_once( ABSPATH . 'wp-admin/admin-header.php');
-            echo $data;
-            include( ABSPATH . 'wp-admin/admin-footer.php');
-            exit;
-        }
-        return;
-  }
-
   public static function delete_directory( $path, $permalink, $recursive = false ) {
     if (!$path)
 	return false;
     global $wp_filesystem;
-    if ( self::$filesystem == null ) {
+    if ( self::$filesystem == null || $this->path !== $path ) {
         self::$filesystem = new Filesystem_Helper( $path );
-    } else {
-	self::$filesystem->set_path( $path );
-    }
-    return $wp_filesystem->delete( self::$filesystem->get_nginx_cache_path( $permalink ), $recursive );
+        self::$filesystem->initialize_filesystem();
+    } 
+    return $wp_filesystem->rmdir( self::$filesystem->get_nginx_cache_path( $permalink ), $recursive );
   }
 
   private function get_nginx_cache_path ( $permalink ) {
